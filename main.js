5bfb26d06b9d81673c58e7f53979ea56cdb914ae
zhiyu <zhiyuwu96@gmail.com>
2019-09-17 17:31:27 +0800

    20190917-2

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-var app = new Vue({
+/*var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
     }
-})
+})*/
+new Vue({
+    el: "#app",
+    data: {
+      todos: [
+        { text: "Learn JavaScript", done: false },
+        { text: "Learn Vue", done: false },
+        { text: "Play around in JSFiddle", done: true },
+        { text: "Build something awesome", done: true }
+      ]
+    },
+    methods: {
+        toggle: function(todo){
+          todo.done = !todo.done
+      }
+    }
+  })
